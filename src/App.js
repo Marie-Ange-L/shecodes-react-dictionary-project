bd55ce8829a92ae666803e39d88ad79570892784
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2023-05-24 11:19:40 +0200

    Created Search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
 	return (
 		<div className="App">
-			<header className="App-header">Hello</header>
+			<header>Dictionary</header>
+			<main>
+				<Dictionary />
+			</main>
+			<footer>
+				SheCodes project coded by{" "}
+				<a
+					href="https://www.linkedin.com/in/marie-ange-lhernould/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Marie-Ange L.
+				</a>{" "}
+				— See on{" "}
+				<a
+					href="https://github.com/Marie-Ange-L/shecodes-react-dictionary-project"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+				.
+			</footer>
 		</div>
 	);
 }
