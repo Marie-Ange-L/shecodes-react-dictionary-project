f39c1946db7e1ccef17d4b70446782b4385a3311
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2023-05-24 11:53:29 +0200

    Intagrated Dictionary API
    using SheCodes API https://www.shecodes.io/learn/apis/dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
 	let [keyword, setKeyword] = useState("");
 
+	function handleResponse(response) {
+		console.log(response.data);
+	}
+
 	function search(event) {
 		event.preventDefault();
-		alert(`Searching for the definition of ${keyword}`);
+		const apiKey = "c0ca36f1te5cd0bd24a89c30e4524bbo";
+		let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+		axios.get(apiUrl).then(handleResponse);
 	}
 
 	function handleKeywordChange(event) {
