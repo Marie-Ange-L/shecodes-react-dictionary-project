6247aa54dd34ce22fa82ad1d865406083e263488
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2023-06-10 10:38:22 +0200

    Updated styling

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
 import React from "react";
 import "./App.css";
 import Dictionary from "./Dictionary";
+import logo from "./bookshelf.png";
 
 export default function App() {
 	return (
 		<div className="App">
-			<header>Dictionary</header>
+			<header>
+				<img src={logo} alt="Logo" className="App-logo img-fluid" />
+			</header>
 			<main>
 				<Dictionary />
 			</main>
 			<footer>
-				SheCodes project coded by{" "}
-				<a
-					href="https://www.linkedin.com/in/marie-ange-lhernould/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Marie-Ange L.
-				</a>{" "}
-				— See on{" "}
-				<a
-					href="https://github.com/Marie-Ange-L/shecodes-react-dictionary-project"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-				.
+				<p>
+					SheCodes project coded by{" "}
+					<a
+						href="https://www.linkedin.com/in/marie-ange-lhernould/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Marie-Ange L.
+					</a>{" "}
+					— See on{" "}
+					<a
+						href="https://github.com/Marie-Ange-L/shecodes-react-dictionary-project"
+						target="_blank"
+						rel="noreferrer"
+					>
+						GitHub
+					</a>
+					.
+				</p>
+				<p className="App-img-credit">
+					•{" "}
+					<a
+						href="https://www.freepik.com/free-vector/flat-colorful-books-plants-stand-bookshelf_37476481.htm#query=illustrations%20bookshelf&position=42&from_view=search&track=ais"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Image by redgreystock
+					</a>{" "}
+					on Freepik •
+				</p>
 			</footer>
 		</div>
 	);
